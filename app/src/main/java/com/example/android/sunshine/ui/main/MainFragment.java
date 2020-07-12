8ba9ce4415c0b95b096034a434f782e4ec927f96
Antonio da Paixao <aquilesdapaixao@gmail.com>
2020-07-12 01:24:26 +0100

    Show list view with mock data

--- app/src/main/java/com/example/android/sunshine/ui/main/MainFragment.java
+++ app/src/main/java/com/example/android/sunshine/ui/main/MainFragment.java
@@ -11,12 +11,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.android.sunshine.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+
+    private ArrayAdapter<String> forecastAdapter;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -26,7 +33,33 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+
+        ArrayList<String> weekForecast = new ArrayList<String>();
+
+
+        weekForecast.addAll( Arrays.asList(
+                "Seg - Ensolarado - 40/32",
+                "Ter - Frio - 32/02",
+                "Qua - Húmido - 24/03",
+                "Qui - Ensolarado - 33/30",
+                "Sex - Ensorado - 23/21",
+                "Sab - Chuva - 15/09",
+                "Dom - Sereno - 16/10"));
+
+        forecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+
+        ListView listViewForecast = (ListView)rootView.findViewById(R.id.list_view_forecast);
+
+        listViewForecast.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 
     @Override
